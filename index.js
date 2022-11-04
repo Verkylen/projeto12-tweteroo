b806d75055b8474598fbec1aec30b511fb1754c2
Renato da Silva Viana <verkylen@gmail.com>
2022-11-04 20:17:27 -0300

    Added data validation

--- index.js
+++ index.js
@@ -12,24 +12,64 @@ const tweets = [];
 
 let currentAvatar = '';
 
+function check(req, res, property) {
+    const {body} = req;
+
+    const firstConditions = [
+        Object.keys(body).length == 2,
+        body.hasOwnProperty('username'),
+        body.hasOwnProperty(property),
+        typeof(body.username) === 'string',
+        typeof(body[property]) === 'string'
+    ];
+
+    const secondConditions = [
+        body.username !== '',
+        body[property] !== ''
+    ];
+
+    if ( !firstConditions.reduce((prev, curr) => prev && curr) ) {
+        res.sendStatus(400);
+        return false;
+    }
+
+    if ( !secondConditions.reduce((prev, curr) => prev && curr) ) {
+        res.status(400);
+        res.send('Todos os campos são obrigatórios!');
+        return false;
+    }
+
+    return true;
+}
+
 app.post('/sign-up', (req, res) => {
     const {body} = req;
+
+    if (!check(req, res, 'avatar')) {
+        return;
+    }
+
     body['id'] = users.length;
 
     users.push(body);
     currentAvatar = body.avatar;
 
-    res.send('OK');
+    res.status(201).send('OK');
 });
 
 app.post('/tweets', (req, res) => {
     const {body} = req;
+
+    if (!check(req, res, 'tweet')) {
+        return;
+    }
+
     body['id'] = tweets.length;
     body['avatar'] = currentAvatar;
 
     tweets.push(body);
 
-    res.send('OK');
+    res.status(201).send('OK');
 });
 
 app.get('/tweets', ({}, res) => {
